b89415dc617a0ab33b8896d0bddac7c74e5b77b0
skid <skid@skids-MacBook-Pro.local>
2022-12-18 19:10:16 +0100

    counting songs seems to work

---
+++ main.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const async = require('async');
+
+const BASE_URL = 'http://keygenmusic.net/';
+const MAX_CONCURRENT_REQUESTS = 10;
+
+const countSongs = async href => {
+  const res = await axios.get(BASE_URL + href);
+	const $ = cheerio.load(res.data);
+	$.html();
+	const table = $('table.teamtable')[0];
+	const tr = table.children[0].children;
+	return (tr.length - 1) / 2;
+}
+
+const getHomepage = async () => {
+  const res = await axios.get(BASE_URL);
+  return res.data;
+}
+
+// main
+(async () => {
+  const homepageHTML = await getHomepage();
+  const $ = cheerio.load(homepageHTML);
+  $.html();
+
+  const menu = $('.menucontainer > ul')[0];
+  const artists = $(menu).children('li');
+  let hrefs = Array.from(
+    artists.map((_, a) => a.children[0].attribs.href)
+  );
+  hrefs = [ hrefs[1], hrefs[2], hrefs[3] ];
+
+  async.mapLimit(hrefs, MAX_CONCURRENT_REQUESTS, countSongs)
+  .then(res => {
+    console.log("OK");
+    console.log(res.reduce((a,b)=>a+b, 0));
+  })
+  .catch(console.log);
+})();
+
+// Promise.all(
+	// hrefs.map(href => axios.get(baseURL + href))
+// ).then(responses => {
+	// //console.log(responses[0]);
+	// let songCounts = responses.map(r => countSongs(r.data));
+	// let total = songCounts.reduce((a,b) => a+b, 0);
+	// console.log(total);
+// });
